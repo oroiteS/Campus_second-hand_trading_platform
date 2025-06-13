766604451cfca7a9d16d999cd59bbcfd0bf1e30a
oroiteS <oroite@qq.com>
2025-06-13 11:22:39 +0800

    update backend/sql

---
+++ backend/sql/database.sql
@@ -0,0 +1,2 @@
+CREATE database campus;
+use campus;
--- backend/sql/user.sql
+++ backend/sql/user.sql
@@ -1,4 +1,3 @@
-CREATE database campus;
 use campus;
 CREATE TABLE IF NOT EXISTS `users` (
     `id` INT AUTO_INCREMENT PRIMARY KEY COMMENT '自增ID',
