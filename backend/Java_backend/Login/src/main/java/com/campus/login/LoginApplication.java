903b0f04fff3032bfea3ca32b33f5e4bfc7b4d4e
forg1ve1125 <lyc031125@outlook.com>
2025-06-14 13:44:20 +0800

    Create feture/login

---
+++ backend/Java_backend/Login/src/main/java/com/campus/login/LoginApplication.java
@@ -0,0 +1,13 @@
+package com.campus.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+
+}
