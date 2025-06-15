340cc7dcf59f0cd16b8a02a1a1421458e11bca69
forg1ve1125 <lyc031125@outlook.com>
2025-06-15 10:24:03 +0800

    新增Ban功能

---
+++ backend/Java_backend/Ban/src/main/java/com/example/ban/BanApplication.java
@@ -0,0 +1,13 @@
+package com.example.ban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanApplication.class, args);
+    }
+
+}
---
+++ backend/Java_backend/Ban/src/test/java/com/example/ban/BanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
