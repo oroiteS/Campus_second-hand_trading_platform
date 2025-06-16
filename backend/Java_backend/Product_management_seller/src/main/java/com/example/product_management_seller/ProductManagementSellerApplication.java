6800fc2a325c807684472ffae0178c12d51aa650
forg1ve1125 <lyc031125@outlook.com>
2025-06-16 11:33:48 +0800

    创建文件

---
+++ backend/Java_backend/Product_management_seller/src/main/java/com/example/product_management_seller/ProductManagementSellerApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_management_seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementSellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductManagementSellerApplication.class, args);
+    }
+
+}
