a3519111cb4f7ee442ccce08ccb90b9619223bd7
forg1ve1125 <lyc031125@outlook.com>
2025-06-16 23:37:56 +0800

    添加查看最新商品功能

---
+++ backend/Java_backend/View_latest/src/main/java/com/campus/view_latest/ViewLatestApplication.java
@@ -0,0 +1,13 @@
+package com.campus.view_latest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewLatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewLatestApplication.class, args);
+    }
+
+}
---
+++ backend/Java_backend/View_latest/src/test/java/com/campus/view_latest/ViewLatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.view_latest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewLatestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
